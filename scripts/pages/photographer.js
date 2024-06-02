dbb7aca7f426a6166ba280b234bd87cbbcfde134
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2024-06-02 10:03:30 +0200

    get user ID and fetch data using ID in URL, adding a loop to show user media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,130 +1,102 @@
-//Mettre le code JavaScript lié à la page photographer.html
-let url = location.href.split('?')
-
-// On récupère les variables depuis l'URL
-const userNameNotClean = url[url.length -6]
-const idImg = url[url.length -5]
-const city = url[url.length -4]
-const country = url[url.length -3]
-const taglineNotClean = url[url.length -2]
-const price = url[url.length -1]
-
-// On clean les variables avec des espaces
-const userName = userNameNotClean.replace(/%20/g, ' ');
-const tagline = taglineNotClean.replace(/%20/g, ' ');
-
-// On créer le lien repo de la photo de profile
-const pictureProfile = `assets/photographers/id_photos/${idImg}`;
-
-// Fonction qui renvoie que les données utilisateur ( type=text )
-function userInfos() {
-    // Créer l'élément div
-    const div = document.createElement('div');
-    div.classList.add('user-info');
-
-    // Créer l'élément h2 = Nom de l'utilisateur
-    const h2_UserName = document.createElement('h2');
-    h2_UserName.textContent = userName;
-
-    // Créer l'élément h3 = région + pays
-    const h3_country = document.createElement('h3');
-    h3_country.textContent = `${city}, ${country}`;
-
-    // Créer l'élément p = tagline
-    const p_tagline = document.createElement('p');
-    p_tagline.textContent = tagline;
-
-    // Créer l'élément span = price
-    const span_price = document.createElement('span');
-    span_price.textContent = price;
-
-    // Ajouter les élément à la div
-    div.appendChild(h2_UserName);
-    div.appendChild(h3_country);
-    div.appendChild(p_tagline);
-
-    // Retourner la div complète
-    return div;
+// Fonction pour récupérer l'ID de l'utilisateur à partir de l'URL
+function getUserIDFromURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('id');
 }
 
-// fonction qui renvoie la photo de profile de la personne
-function userPicture() {
-    // Créer l'élément div
-    const div = document.createElement('div');
+async function displayPhotographerInfo() {
+    const userID = getUserIDFromURL();
 
-    // Créer l'élément img
-    const img = document.createElement('img');
-    img.setAttribute("src", pictureProfile);
-    img.classList.add('user-picture');
+    if (userID) {
+        const photographers = await getPhotographerInfos()
 
-    // Ajouter l'élément img à la div
-    div.appendChild(img);
+        if (photographers) {
+            const photographer = photographers.photographers.find(p => p.id === parseInt(userID))
 
-    // Retourner la div complète
-    return div;
+            const media = photographers.media.filter(m => m.photographerId === parseInt(userID))
+
+            const picture = `assets/photographers/id_photos/${photographer.portrait}`
+
+            if (photographer) {
+                // selectionner les classes des div
+                const photographersName = document.querySelector(".user_infos")
+                const photographersPicture = document.querySelector(".user_picture")
+                const photographersImages = document.querySelector(".photograph-images")
+
+                // créer un h2 comme nom du photographe
+                const userName = document.createElement( 'h2')
+                userName.textContent = photographer.name
+
+                // créer un p comme Lieu de naissance
+                const userCountry = document.createElement( 'p')
+                userCountry.textContent = photographer.city + ", " + photographer.country
+
+                // créer un p comme Lieu de naissance
+                const userQuote = document.createElement( 'p')
+                userQuote.textContent = photographer.tagline
+
+                // créer l'element photo de profile
+                const userPicture = document.createElement( 'img');
+                userPicture.setAttribute("src", picture)
+                userPicture.classList.add('user-picture')
+
+                // console.log(userPicture)
+
+                // appendChild le nom et la photo de profile
+                photographersName.appendChild(userName)
+                photographersName.appendChild(userCountry)
+                photographersName.appendChild(userQuote)
+                photographersPicture.appendChild(userPicture)
+                // console.log(photographer)
+
+                // selectionner le prénom de l'utilisateur et le transformer en lowercase
+                const photographerFirstName = photographer.name.split(' ')[0].toLowerCase().replace("-", "_")
+                console.log(photographerFirstName)
+
+                // créer le nom du dossier au nom de l'utilisatuer ou sont stocker les images
+                const mediaFolder = `assets/images/${photographerFirstName}`
+
+                // créer une boucle pour afficher les images
+                // media.forEach(media => {
+                //     const imgElement = document.createElement("img");
+                //     imgElement.src = `${mediaFolder}/${media.image}`;
+                //     imgElement.alt = `${media.title}`;
+                //     imgElement.classList.add('user-picture')
+                //     photographersImages.appendChild(imgElement);
+                // });
+
+
+                // mettre en Class - a faire
+                media.forEach(media => {
+                    if (media.image) {
+                        const article = document.createElement("article")
+                        const imgElement = document.createElement("img");
+                        imgElement.src = `${mediaFolder}/${media.image}`;
+                        imgElement.alt = `${media.title}`;
+                        imgElement.classList.add('file-media-grid')
+                        photographersImages.appendChild(article).appendChild(imgElement);
+                    } else if (media.video) {
+                        const article = document.createElement("article")
+                        const videoElement = document.createElement("video");
+                        videoElement.src = `${mediaFolder}/${media.video}`
+                        videoElement.classList.add('file-media-grid')
+                        videoElement.controls = false
+                        photographersImages.appendChild(article).appendChild(videoElement);
+                    }
+                });
+
+            } else {
+                console.log(`Photographe avec l'ID ${userID} non trouvé.`);
+            }
+        } else {
+            console.log('Impossible de récupérer les données des photographes.');
+        }
+    } else {
+        console.log('ID utilisateur non trouvé dans l URL.');
+    }
 }
 
 
-
-
-
-
-
-// Récuperer le prénom d'utilisateur
-let userNameFull = userName.split(' ')
-const userNickNameCleaned = userNameFull[userNameFull.length -2]
-
-// Rendre le prénom en Lowercase
-const userNickNameLowerCase = userNickNameCleaned.toLowerCase()
-
-// Créer le lien repo du dossier images du photographe
-const pictureFolder = `assets/images/${userNickNameLowerCase}`;
-console.log(pictureFolder)
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function displayUserInfos(){
-    // Sélectionner la div.user_infos
-    const photographerInfos = document.querySelector(".user_infos");
-    photographerInfos.appendChild(userInfos());
-
-    // Sélectionner la div.user_picture
-    const photographerPicture = document.querySelector(".user_picture");
-    photographerPicture.appendChild(userPicture())
-}
-
-displayUserInfos()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Another way of getting a URL ID
-let id = new URLSearchParams(window.location.search).get("id");
-// console.log(id)
+// Appeler la fonction pour afficher les informations du photographe
+displayPhotographerInfo();
 
