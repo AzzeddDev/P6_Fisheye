8fa32df092ffcbda858b887203cffd13b1c80b8d
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2024-06-08 23:21:49 +0200

    change to querySelectorAll, add photographer price & update js code

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,5 @@
+import {MediaFactory} from "../factory/mediaFactory.js"
+
 // Fonction pour récupérer l'ID de l'utilisateur à partir de l'URL
 function getUserIDFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
@@ -8,91 +10,68 @@ async function displayPhotographerInfo() {
     const userID = getUserIDFromURL();
 
     if (userID) {
-        const photographers = await getPhotographerInfos()
+        const photographersData = await getPhotographerInfos()
 
-        if (photographers) {
-            const photographer = photographers.photographers.find(p => p.id === parseInt(userID))
+        if (photographersData) {
+            const photographerData = photographersData.photographers.find(p => p.id === parseInt(userID))
+            const mediaList = photographersData.media.filter(m => m.photographerId === parseInt(userID))
 
-            const media = photographers.media.filter(m => m.photographerId === parseInt(userID))
+            if (photographerData) {
 
-            const picture = `assets/photographers/id_photos/${photographer.portrait}`
-
-            if (photographer) {
-                // selectionner les classes des div
-                const photographersName = document.querySelector(".user_infos")
-                const photographersPicture = document.querySelector(".user_picture")
+                // Sélectionner les classes des div
+                const photographerNodeList = document.querySelectorAll(".user_infos, .user_picture, .photograph-price")
                 const photographersImages = document.querySelector(".photograph-images")
+
+                // créer une variable pour le recup le lien de la photo de profile de l'utilisateur
+                const picture = `assets/photographers/id_photos/${photographerData.portrait}`
 
                 // créer un h2 comme nom du photographe
                 const userName = document.createElement( 'h2')
-                userName.textContent = photographer.name
+                userName.textContent = photographerData.name
 
                 // créer un p comme Lieu de naissance
                 const userCountry = document.createElement( 'p')
-                userCountry.textContent = photographer.city + ", " + photographer.country
+                userCountry.textContent = photographerData.city + ", " + photographerData.country
 
                 // créer un p comme Lieu de naissance
                 const userQuote = document.createElement( 'p')
-                userQuote.textContent = photographer.tagline
+                userQuote.textContent = photographerData.tagline
 
                 // créer l'element photo de profile
                 const userPicture = document.createElement( 'img');
                 userPicture.setAttribute("src", picture)
                 userPicture.classList.add('user-picture')
 
-                // console.log(userPicture)
+                // créer l'element user price
+                const userPrice = document.createElement("span")
+                userPrice.textContent = photographerData.price + `€ / jour`
 
                 // appendChild le nom et la photo de profile
-                photographersName.appendChild(userName)
-                photographersName.appendChild(userCountry)
-                photographersName.appendChild(userQuote)
-                photographersPicture.appendChild(userPicture)
-                // console.log(photographer)
+                photographerNodeList[0].appendChild(userName)
+                photographerNodeList[0].appendChild(userCountry)
+                photographerNodeList[0].appendChild(userQuote)
+                photographerNodeList[1].appendChild(userPicture)
+                photographerNodeList[2].appendChild(userPrice)
 
                 // selectionner le prénom de l'utilisateur et le transformer en lowercase
-                const photographerFirstName = photographer.name.split(' ')[0].toLowerCase().replace("-", "_")
-                console.log(photographerFirstName)
+                const photographerFirstName = photographerData.name.split(' ')[0].toLowerCase().replace("-", "_")
 
                 // créer le nom du dossier au nom de l'utilisatuer ou sont stocker les images
                 const mediaFolder = `assets/images/${photographerFirstName}`
 
-                // créer une boucle pour afficher les images
-                // media.forEach(media => {
-                //     const imgElement = document.createElement("img");
-                //     imgElement.src = `${mediaFolder}/${media.image}`;
-                //     imgElement.alt = `${media.title}`;
-                //     imgElement.classList.add('user-picture')
-                //     photographersImages.appendChild(imgElement);
-                // });
-
-
-                // mettre en Class - a faire
-                media.forEach(media => {
-                    if (media.image) {
-                        const article = document.createElement("article")
-                        const imgElement = document.createElement("img");
-                        imgElement.src = `${mediaFolder}/${media.image}`;
-                        imgElement.alt = `${media.title}`;
-                        imgElement.classList.add('file-media-grid')
-                        photographersImages.appendChild(article).appendChild(imgElement);
-                    } else if (media.video) {
-                        const article = document.createElement("article")
-                        const videoElement = document.createElement("video");
-                        videoElement.src = `${mediaFolder}/${media.video}`
-                        videoElement.classList.add('file-media-grid')
-                        videoElement.controls = false
-                        photographersImages.appendChild(article).appendChild(videoElement);
-                    }
-                });
+                // créer une boucle en Factory des medias
+                mediaList.forEach(media => {
+                    new MediaFactory(media, mediaFolder, photographersImages)
+                })
 
             } else {
-                console.log(`Photographe avec l'ID ${userID} non trouvé.`);
+                console.log(`Photographe avec l'ID ${userID} non trouvé.`)
             }
         } else {
-            console.log('Impossible de récupérer les données des photographes.');
+            console.log('Impossible de récupérer les données des photographes.')
         }
     } else {
-        console.log('ID utilisateur non trouvé dans l URL.');
+        console.log('ID utilisateur non trouvé dans l URL.')
     }
 }
 
