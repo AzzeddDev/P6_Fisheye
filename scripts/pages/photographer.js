4efb7d1977fd9b1dfe5ee26d71795faf8356e255
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2024-06-12 20:03:12 +0200

    new operator for both Photographer header and medias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,15 @@
 import {MediaFactory} from "../factory/mediaFactory.js"
+import {PhotographerHeader} from "../class/photographerHeader.js"
+import {PhotographerMedias} from "../class/photographerMedias.js"
 
 // Fonction pour récupérer l'ID de l'utilisateur à partir de l'URL
 function getUserIDFromURL() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('id');
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get('id')
 }
 
 async function displayPhotographerInfo() {
-    const userID = getUserIDFromURL();
+    const userID = getUserIDFromURL()
 
     if (userID) {
         const photographersData = await getPhotographerInfos()
@@ -22,47 +24,12 @@ async function displayPhotographerInfo() {
                 const photographerNodeList = document.querySelectorAll(".user_infos, .user_picture, .photograph-price")
                 const photographersImages = document.querySelector(".photograph-images")
 
-                // créer une variable pour le recup le lien de la photo de profile de l'utilisateur
-                const picture = `assets/photographers/id_photos/${photographerData.portrait}`
+                // Generer un Header avec les infos du photograph
+                new PhotographerHeader(photographerData, photographerNodeList)
 
-                // créer un h2 comme nom du photographe
-                const userName = document.createElement( 'h2')
-                userName.textContent = photographerData.name
+                // Generer les Medias du photograph
+                new PhotographerMedias(photographerData, mediaList, photographersImages)
 
-                // créer un p comme Lieu de naissance
-                const userCountry = document.createElement( 'p')
-                userCountry.textContent = photographerData.city + ", " + photographerData.country
-
-                // créer un p comme Lieu de naissance
-                const userQuote = document.createElement( 'p')
-                userQuote.textContent = photographerData.tagline
-
-                // créer l'element photo de profile
-                const userPicture = document.createElement( 'img');
-                userPicture.setAttribute("src", picture)
-                userPicture.classList.add('user-picture')
-
-                // créer l'element user price
-                const userPrice = document.createElement("span")
-                userPrice.textContent = photographerData.price + `€ / jour`
-
-                // appendChild le nom et la photo de profile
-                photographerNodeList[0].appendChild(userName)
-                photographerNodeList[0].appendChild(userCountry)
-                photographerNodeList[0].appendChild(userQuote)
-                photographerNodeList[1].appendChild(userPicture)
-                photographerNodeList[2].appendChild(userPrice)
-
-                // selectionner le prénom de l'utilisateur et le transformer en lowercase
-                const photographerFirstName = photographerData.name.split(' ')[0].toLowerCase().replace("-", "_")
-
-                // créer le nom du dossier au nom de l'utilisatuer ou sont stocker les images
-                const mediaFolder = `assets/images/${photographerFirstName}`
-
-                // créer une boucle en Factory des medias
-                mediaList.forEach(media => {
-                    new MediaFactory(media, mediaFolder, photographersImages)
-                })
 
             } else {
                 console.log(`Photographe avec l'ID ${userID} non trouvé.`)
@@ -75,7 +42,6 @@ async function displayPhotographerInfo() {
     }
 }
 
-
 // Appeler la fonction pour afficher les informations du photographe
-displayPhotographerInfo();
+displayPhotographerInfo()
 
